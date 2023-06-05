df7641b0ea88cfb515de32aa8af948053929b674
vyacheslav.khaetskiy <v.a.khaetskiy@gmail.com>
2023-06-05 19:42:35 +0500

    added classes: - TestSelectablePage
    added tests:
    - test_selectable

--- locators/interactions_page_locators.py
+++ locators/interactions_page_locators.py
@@ -7,3 +7,15 @@ class SortablePageLocators:
 
     GRID_TAB = (By.CSS_SELECTOR, 'a[id="demo-tab-grid"]')
     GRID_ITEM = (By.CSS_SELECTOR, 'div[id="demo-tabpane-grid"] div[class="list-group-item list-group-item-action"]')
+
+
+class SelectablePageLocators:
+    LIST_TAB = (By.CSS_SELECTOR, 'a[id="demo-tab-list"]')
+    LIST_ITEM = (By.CSS_SELECTOR, 'div[id="demo-tabpane-list"] li[class="mt-2 list-group-item list-group-item-action"]')
+    LIST_ITEM_ACTIVE = (By.CSS_SELECTOR,
+                        'div[id="demo-tabpane-list"] li[class="mt-2 list-group-item active list-group-item-action"]')
+
+    GRID_TAB = (By.CSS_SELECTOR, 'a[id="demo-tab-grid"]')
+    GRID_ITEM = (By.CSS_SELECTOR, 'div[id="demo-tabpane-grid"] li[class="list-group-item list-group-item-action"]')
+    GRID_ITEM_ACTIVE = (By.CSS_SELECTOR,
+                        'div[id="demo-tabpane-grid"] li[class="list-group-item active list-group-item-action"]')
--- tests/interactions_test.py
+++ tests/interactions_test.py
@@ -1,4 +1,4 @@
-from pages.interactions_page import SortablePage
+from pages.interactions_page import SortablePage, SelectablePage
 
 
 class TestInteractions:
@@ -10,3 +10,12 @@ class TestInteractions:
             grid_before, grid_after = sortable_page.change_grid_order()
             assert list_before != list_after, 'List Order has not been changed'
             assert grid_before != grid_after, 'Grid Order has not been changed'
+
+    class TestSelectablePage:
+        def test_selectable(self, driver):
+            selectable_page = SelectablePage(driver, 'https://demoqa.com/selectable')
+            selectable_page.open()
+            item_list = selectable_page.select_list_item()
+            item_grid = selectable_page.select_grid_item()
+            assert len(item_list) > 0, 'List Tab element has not been selected'
+            assert len(item_grid) > 0, 'Grid Tab element has not been selected'
